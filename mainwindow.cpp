8505c74c4340818ffe4c4affd7fcc1c077b8e2f9
ChrisLinux127 <81356792+ChrisLinux127@users.noreply.github.com>
2021-03-26 12:43:54 +0100

    Added Cos, Tan Plots , Amplitude, Frequenzy and index of plot

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include<iostream>
+#include<math.h>
 //Konstruktor:
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,14 +12,19 @@ MainWindow::MainWindow(QWidget *parent)
     ui->bx_xMin->setMinimum(std::numeric_limits<double>::lowest());
     ui->bx_yMin->setMinimum(std::numeric_limits<double>::lowest());
 
+    //STD Var Settings:
+    A=1;    //1
+    Omega=M_PI*10;  //10 Hz
+    graph_index=0;
+
     //Plot Einstellungen:
     ui->PlotView->addGraph(); //Graph hinzufügen
-    ui->PlotView->graph(0)->setScatterStyle(QCPScatterStyle::ssCross); //Einstellung von Graph 0
-    ui->PlotView->graph(0)->setLineStyle(QCPGraph::lsNone);
+    ui->PlotView->graph(graph_index)->setScatterStyle(QCPScatterStyle::ssCross); //Einstellung von Graph 0
+    //ui->PlotView->graph(0)->setLineStyle(QCPGraph::lsNone);
     ui->PlotView->xAxis->setLabel("x");
     ui->PlotView->yAxis->setLabel("f(x)");
-    ui->PlotView->xAxis->setRange(-5,5); //STandard
-    ui->PlotView->yAxis->setRange(-5,5);
+    ui->PlotView->xAxis->setRange(-10,10); //STandard
+    ui->PlotView->yAxis->setRange(-10,10);
 }
 //Destruktor:
 MainWindow::~MainWindow()
@@ -30,6 +36,7 @@ void MainWindow::addPunkt(double x, double y)
 {
     Vect_X.append(x);
     Vect_Y.append(y);
+    ui->PlotView->graph(graph_index)->setData(Vect_X,Vect_Y);
 }
 
 void MainWindow::loeschen()
@@ -40,17 +47,75 @@ void MainWindow::loeschen()
 
 void MainWindow::zeichne()
 {
-    ui->PlotView->graph(0)->setData(Vect_X,Vect_Y);
     ui->PlotView->replot();
     ui->PlotView->update();
 }
 
 //Btn Events:
 
-void MainWindow::on_pushButton_clicked()
+//Plots:
+void MainWindow::on_rbnt_sin_clicked()
 {
-ui->PlotView->xAxis->setRange(ui->bx_xMin->value(),ui->bx_xMax->value());
-ui->PlotView->yAxis->setRange(ui->bx_yMin->value(),ui->bx_yMin->value());
-ui->PlotView->replot();
-ui->PlotView->update();
+    Vect_X.clear();
+    Vect_Y.clear();
+    for(double i=0;i<10;i=i+0.5)
+    {
+
+       addPunkt(i,sin(i));
+    }
+    zeichne();
+}
+
+void MainWindow::on_rbnt_cos_clicked()
+{
+    Vect_X.clear();
+    Vect_Y.clear();
+    for(double i=0;i<10;i=i+0.5)
+    {
+
+       addPunkt(i,cos(i));
+    }
+    zeichne();
+}
+void MainWindow::on_rbnt_tan_clicked()
+{
+    Vect_X.clear();
+    Vect_Y.clear();
+    for(double i=0;i<10;i=i+0.5)
+    {
+
+       addPunkt(i,tan(i));
+    }
+    zeichne();
+}
+
+//Fenster einstellungen:
+void MainWindow::on_btn_settings_clicked()
+{
+    ui->PlotView->replot();
+    ui->PlotView->update();
+}
+
+
+
+//Xmax:
+void MainWindow::on_bx_xMax_valueChanged(double arg1)
+{
+    ui->PlotView->xAxis->setRangeUpper(arg1);
+}
+//Ymax:
+void MainWindow::on_bx_YMax_valueChanged(double arg1)
+{
+    ui->PlotView->yAxis->setRangeUpper(arg1);
+}
+//Xmin:
+void MainWindow::on_bx_xMin_valueChanged(double arg1)
+{
+    ui->PlotView->xAxis->setRangeLower(arg1);
+}
+//Ymin:
+
+void MainWindow::on_bx_yMin_valueChanged(double arg1)
+{
+    ui->PlotView->yAxis->setRangeLower(arg1);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -18,13 +18,27 @@ public:
      void loeschen();   //Punkt loeschen
      void zeichne();    //Zeichnen
 private slots:
+     void on_rbnt_sin_clicked();
+
+     void on_rbnt_cos_clicked();
+
+     void on_btn_settings_clicked();
+
+     void on_bx_xMax_valueChanged(double arg1);
+
+     void on_bx_YMax_valueChanged(double arg1);
+
+     void on_bx_xMin_valueChanged(double arg1);
+
+     void on_bx_yMin_valueChanged(double arg1);
 
 
-
-
-     void on_pushButton_clicked();
+     void on_rbnt_tan_clicked();
 
 private:
+    int graph_index;
+    double A;
+    double Omega;
     Ui::MainWindow *ui;
     QVector<double>Vect_X,Vect_Y;
 };
